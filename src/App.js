46dcdfe696d060b1e8642a1eb197f11757b608ef
rafacidade <30698258+rafacidade@users.noreply.github.com>
2018-12-16 14:48:00 -0200

    Indent repairing

--- src/App.js
+++ src/App.js
@@ -8,109 +8,111 @@ import bookNoImage from './images/book-noimage.png'
 
 class BooksApp extends React.Component {
 	state = {
-    books: [],
-    searchBooksResults: [],
-    searchTerms: ''
-  }
+		books: [],
+		searchBooksResults: [],
+		searchTerms: ''
+	}
 
 	componentDidMount() {
 		BooksAPI.getAll().then((response) => {
-      this.setState({ books: this.normalizeBooks(response) })
-    })    
-  }
-  
-  //complete book object if necessary
-  normalizeBooks = (books) => {
-		return books.map(book => {
-      if (book.authors === undefined) {
-        book = {...book, authors: [] }
-      }
-      if (book.shelf === undefined) {
-        book = {...book, shelf: 'none' }
-      }
-      if (book.imageLinks === undefined) {
-        book = {...book, imageLinks: { smallThumbnail: bookNoImage } }
-      }
-      return book
-    })
+			this.setState({ books: this.normalizeBooks(response) })
+		})
 	}
 
-  getShelfBooks = (shelf) => {
+	//complete book object if necessary
+	normalizeBooks = (books) => {
+		return books.map(book => {
+			if (book.authors === undefined) {
+				book = {...book, authors: [] }
+			}
+			if (book.shelf === undefined) {
+				book = {...book, shelf: 'none' }
+			}
+			if (book.imageLinks === undefined) {
+				book = {...book, imageLinks: { smallThumbnail: bookNoImage } }
+			}
+			return book
+		})
+	}
+	
+	getShelfBooks = (shelf) => {
 		return this.state.books.filter((book) => book.shelf === shelf)
 	}
-
+	
 	moveBook = (book, shelf) => {
-    if(book.shelf !== shelf) {
-      BooksAPI.update(book, shelf).then( response => {
-        book.shelf = shelf;
-
-        this.setState(previousState => ({
-          books: [ ...previousState.books.filter(b => b.id !== book.id), book ]
-        }))
-      })
-    }    
+		if(book.shelf !== shelf) {
+			BooksAPI.update(book, shelf).then( response => {
+				book.shelf = shelf;
+				this.setState(previousState => ({
+					books: [ ...previousState.books.filter(b => b.id !== book.id), book ]
+				}))
+			})
+		}
 	}
-
-  searchBooks = (query) => {
-    //update search terms and clear previous results
-    this.setState({ searchTerms: query })
-    this.setState({ searchBooksResults: [] })
-
-    if(query) {
-      BooksAPI.search( query.trim() ).then( response => {
-        if(!response.error) {
-          this.setState({ searchBooksResults: this.normalizeBooks(response) })
-        }
-      })
-    }
-  }
-
+	
+	searchBooks = (query) => {
+		//update search terms and clear previous results
+		this.setState({ searchTerms: query })
+		this.setState({ searchBooksResults: [] })
+		
+		if(query) {
+			BooksAPI.search( query.trim() ).then( response => {
+				if(!response.error) {
+					this.setState({ searchBooksResults: this.normalizeBooks(response) })
+				}
+			})
+		}
+	}
+	
 	render() {
-    return (
-      <div className="app">
-        <Route path='/search' render={({ history }) => (
-          <Search 
-            searchBooks={this.searchBooks} 
-            moveBook={this.moveBook} 
-            searchBooksResults={this.state.searchBooksResults} 
-            searchTerms={this.state.searchTerms}
-          />        
-        )} 
-        />
-        <Route exact path='/' render={() => (        
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-              	<Shelf 
-              		shelfTitle="Currently Reading"
-              		books={this.getShelfBooks("currentlyReading")}
-              		moveBook={this.moveBook}
-              	/>
-              	<Shelf 
-              		shelfTitle="Want to Read"
-              		books={this.getShelfBooks("wantToRead")}
-              		moveBook={this.moveBook}
-              	/>
-              	<Shelf 
-              		shelfTitle="Read"
-              		books={this.getShelfBooks("read")}
-              		moveBook={this.moveBook}
-              	/>
-              </div>
-            </div>
-
-            <Link to="/search">
-                <div className="open-search"><button>Search</button></div>
-            </Link>            
-          </div>
-        )} 
-        />
-      </div>
-    )
-  }
+		return (
+			<div className="app">
+				<Route 
+					path='/search' 
+					render={({ history }) => (
+						<Search
+							searchBooks={this.searchBooks}
+							moveBook={this.moveBook}
+							searchBooksResults={this.state.searchBooksResults}
+							searchTerms={this.state.searchTerms}
+						/>
+					)}
+				/>
+        		<Route 
+					exact path='/' 
+					render={() => (        
+          				<div className="list-books">
+            				<div className="list-books-title">
+              					<h1>MyReads</h1>
+							</div>
+            				<div className="list-books-content">
+              					<div>
+              						<Shelf 
+										shelfTitle="Currently Reading"
+										books={this.getShelfBooks("currentlyReading")}
+										moveBook={this.moveBook}
+              						/>
+									<Shelf 
+										shelfTitle="Want to Read"
+										books={this.getShelfBooks("wantToRead")}
+										moveBook={this.moveBook}
+									/>
+									<Shelf 
+										shelfTitle="Read"
+										books={this.getShelfBooks("read")}
+										moveBook={this.moveBook}
+									/>
+              					</div>
+            				</div>
+							<Link to="/search">
+                				<div className="open-search"><button>Search</button></div>
+							</Link>            
+          				</div>
+        			)} 
+				/>
+      		</div>
+		)
+	}
 }
 
 export default BooksApp
