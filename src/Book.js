fa48654f64fca73594d4f8907d4e984c2080d0e6
rafacidade <30698258+rafacidade@users.noreply.github.com>
2018-12-16 14:33:01 -0200

    Submit Project

--- src/Book.js
+++ src/Book.js
@@ -13,23 +13,23 @@ class Book extends Component {
 		const { book, moveBook } = this.props
 
     return (
-			<div className="book">
-	      <div className="book-top">
-					<div 
-						className="book-cover" 
-						style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}>
-					</div>
-					<ShelfChanger 
-						book={book} 
-						moveBook={moveBook} 
-					/>
-	      </div>
-	      <div className="book-title">{book.title}</div>
-	      <div className="book-authors">
-					<Authors 
-						authors={book.authors}
-					/>
+		<div className="book">
+			<div className="book-top">
+				<div
+					className="book-cover"
+					style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}>
 				</div>
+				<ShelfChanger
+					book={book}
+					moveBook={moveBook}
+				/>
+			</div>
+			<div className="book-title">{book.title}</div>
+			<div className="book-authors">
+				<Authors
+					authors={book.authors}
+				/>
+			</div>
 	    </div>
     )}   
 }
